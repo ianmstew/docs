148fd7bcf6eddc59f995230ef9384acbb939c4dc
Ian Stewart <ianmstew@gmail.com>
2014-06-17 10:49:28 -0400

    Added comments

--- app/app-js/modules/menu/menu.view.js
+++ app/app-js/modules/menu/menu.view.js
@@ -16,13 +16,17 @@ define(function (require) {
       this.listenTo(moduleChannel.vent, 'show:service', function (serviceKey) {
         var serviceModel = self.collection.where({ serviceKey: serviceKey })[0],
             serviceView = self.children.findByModel(serviceModel);
+
         serviceView.triggerMethod('showService');
       });
 
       this.listenTo(moduleChannel.vent, 'select:endpoint', function (serviceKey, endpointKey) {
         var serviceModel = self.collection.where({ serviceKey: serviceKey })[0],
             serviceView = self.children.findByModel(serviceModel);
+
+        // global de-select; see menuItm.view.js#onSelect()
         self.$('.types-selected').removeClass('types-selected');
+
         serviceView.triggerMethod('selectEndpoint', endpointKey);
       });
     }
